b9e22a9ca05acde00c36481fca95d9ad28d0de38
Mogoww <gauthier.mauche@gmail.com>
2022-10-30 17:12:49 +0100

    add test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        Rationnel r1 = new Rationnel(1, 5);
-        Rationnel r2 = new Rationnel(2, 5);
-        r2.plus(r1);
-        System.out.println(r2);
+        Rationnel r1 = new Rationnel(1, 2);
+        Rationnel r2 = new Rationnel(3, 4);
+        r1.plus(r2);
+        System.out.println(r1);
+
+        Rationnel r3 = new Rationnel(-5, 8);
+        Rationnel r4 = new Rationnel(6, 5);
+        Rationnel r5 = new Rationnel(9, 10);
+        r3.fois(r4);
+        r3.division(r5);
+        System.out.println(r3);
+
+        Rationnel r6 = new Rationnel(9, 8);
+        Rationnel r7 = new Rationnel(7, 6);
+        Rationnel r8 = new Rationnel(5, 4);
+        r6.division(r7);
+        r6.soustraction(r8);
+        System.out.println(r6);
     }
 }
