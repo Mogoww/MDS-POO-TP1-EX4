2be559b2b1332707ca126a9ac709c5995f07c9c7
Mogoww <gauthier.mauche@gmail.com>
2022-10-30 11:42:38 +0100

    create PGCD function and constructeur

--- src/Rationnel.java
+++ src/Rationnel.java
@@ -4,8 +4,25 @@ public class Rationnel {
     private int numerateur;
     private int denominateur;
 
+    public Rationnel(int numerateur, int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
     // methodes
     public void normalise(){
-
+        int pgcd = pgcd(numerateur, denominateur);
+        this.numerateur /= pgcd;
+        this.denominateur /= pgcd;
+    }
+    private int pgcd(int a, int b){
+        // calculer le PGCD de a et b
+        int r = a % b;
+        while (r != 0){
+            a = b;
+            b = r;
+            r = a % b;
+        }
+        return b;
     }
 }
